0118e2ce0bd8029011e54ce674595533596afde5
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-08 16:54:30 -0300

    Add Author interface and called they in author service

--- MVCwithSwagger/Services/AuthorServices.cs
+++ MVCwithSwagger/Services/AuthorServices.cs
@@ -1,6 +1,30 @@
-﻿namespace MVCwithSwagger.Services
+﻿using MVCwithSwagger.Data;
+using MVCwithSwagger.Models;
+
+namespace MVCwithSwagger.Services
 {
     public class AuthorServices : IAuthorInterface
     {
+        private readonly AppDBContext _context;
+
+        public AuthorServices(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ResponseModel<List<AuthorModel>>> ListofAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<AuthorModel>> SearchAuthorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<AuthorModel>> SearchAuthorIdBook(int idBook)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MVCwithSwagger/Services/IAuthorInterface.cs
+++ MVCwithSwagger/Services/IAuthorInterface.cs
@@ -1,6 +1,11 @@
-﻿namespace MVCwithSwagger.Services
+﻿using MVCwithSwagger.Models;
+
+namespace MVCwithSwagger.Services
 {
     public interface IAuthorInterface
     {
+        Task<ResponseModel<List<AuthorModel>>> ListofAuthors();
+        Task<ResponseModel<AuthorModel>> SearchAuthorId(int id);
+        Task<ResponseModel<AuthorModel>> SearchAuthorIdBook(int idBook);
     }
 }
