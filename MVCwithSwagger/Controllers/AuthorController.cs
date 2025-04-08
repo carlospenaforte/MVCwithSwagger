9e61ceabb3f99c1c1a900fbc8dc5911773267f00
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-08 17:41:12 -0300

    Authro services calling list of authors with manipulation of exceptions with the method try cath. and authorcontroller making the interface

--- MVCwithSwagger/Controllers/AuthorController.cs
+++ MVCwithSwagger/Controllers/AuthorController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MVCwithSwagger.Models;
+using MVCwithSwagger.Services;
 
 namespace MVCwithSwagger.Controllers
 {
@@ -7,5 +9,19 @@ namespace MVCwithSwagger.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
+        private readonly IAuthorInterface _authorInterface;
+
+        public AuthorController(IAuthorInterface authorInterface)
+        {
+            _authorInterface = authorInterface;
+        }
+
+        [HttpGet("ListofAuthors")]
+        public async Task<ActionResult<ResponseModel<List<AuthorModel>>>> ListAuthors()
+        {
+            var authors = await _authorInterface.ListofAuthors();
+
+            return Ok(authors);
+        }
     }
 }
--- MVCwithSwagger/Services/AuthorServices.cs
+++ MVCwithSwagger/Services/AuthorServices.cs
@@ -1,4 +1,5 @@
-﻿using MVCwithSwagger.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MVCwithSwagger.Data;
 using MVCwithSwagger.Models;
 
 namespace MVCwithSwagger.Services
@@ -12,9 +13,24 @@ namespace MVCwithSwagger.Services
             _context = context;
         }
 
-        public Task<ResponseModel<List<AuthorModel>>> ListofAuthors()
+        public async Task<ResponseModel<List<AuthorModel>>> ListofAuthors()
         {
-            throw new NotImplementedException();
+            ResponseModel<List<AuthorModel>> response = new ResponseModel<List<AuthorModel>>();
+            try
+            {
+                var authors = await _context.Authors.ToListAsync();
+                response.Data = authors;
+                response.Message = "List of authors";
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+
+                return response;
+            }
         }
 
         public Task<ResponseModel<AuthorModel>> SearchAuthorId(int id)
