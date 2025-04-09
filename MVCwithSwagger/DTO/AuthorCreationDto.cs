8c5e41d11002a1ae58937cf4d948f576c2492d3f
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-09 19:25:43 -0300

    starting the PUT method. Creating a task for add author in IAuthorInterface. Create a folder dto with authorcreationdto file class to manipulate the data for the new task model.

---
+++ MVCwithSwagger/DTO/AuthorCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCwithSwagger.DTO
+{
+    public class AuthorCreationDto
+    {
+        public string Name { get; set; }  
+        public string Surname { get; set; } 
+
+    }
+}
--- MVCwithSwagger/Services/IAuthorInterface.cs
+++ MVCwithSwagger/Services/IAuthorInterface.cs
@@ -1,11 +1,18 @@
-﻿using MVCwithSwagger.Models;
+﻿using MVCwithSwagger.DTO;
+using MVCwithSwagger.Models;
 
 namespace MVCwithSwagger.Services
 {
     public interface IAuthorInterface
     {
+        // GET
+
         Task<ResponseModel<List<AuthorModel>>> ListofAuthors();
         Task<ResponseModel<AuthorModel>> SearchAuthorId(int id);
         Task<ResponseModel<AuthorModel>> SearchAuthorIdBook(int idBook);
+
+        // POST
+
+        Task<ResponseModel<AuthorModel>> AddAuhor(AuthorCreationDto authorCreationDto); // You need receive a model
     }
 }
